3649acd7ba65035eb10713fcce7e75ffeb34ab3e
Manishkumarsingh20 <manishkumarsingh1798@gmail.com>
2022-08-09 18:55:22 +0530

    wip

--- adminpanel.php
+++ adminpanel.php
@@ -66,7 +66,7 @@ if (isset($_POST['login'])) {
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
     <?php
-    unset($_SESSION['alert']);
+  
     }
     ?>
 
